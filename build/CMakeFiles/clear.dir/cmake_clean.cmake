bfc7a80e49b9cc81d82f25700022cd1541240660
ajtroup1 <ajtroup1@crimson.ua.edu>
2024-08-23 00:06:13 -0500

    can exit with code via assembly

---
+++ build/CMakeFiles/clear.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/clear.dir/src/main.cpp.o"
+  "CMakeFiles/clear.dir/src/main.cpp.o.d"
+  "clear"
+  "clear.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/clear.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,147 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <optional>
+#include <vector>
+
+// enum class is some C++ wizardry that allows for comparison of integers using string 'identifiers'
+enum class TokenType {
+    _return,
+    int_lit,
+    semi
+};
+
+// Actual token class to be referenced throughout the parser
+// Contains both the type of token and optionally an associated literal value
+struct Token {
+    TokenType type;
+    std::optional<std::string> value;
+};
+
+// function to tokenize entire content string
+// returns a vector of tokens, containing every token in the program
+std::vector<Token> tokenize(std::string& str) {
+    std::vector<Token> tokens;
+    std::string buf; // buffer for containing a sequence of chars to be analyzed and used for tokenization at a later time
+
+    // read through the source code char by char
+    for (int i = 0; i < str.length(); i++) {
+        char c = str.at(i);
+
+        // if the character is alphabetic, it must be a keyword or identifier
+        // this cannot start with a number, but can continue with a number
+        if (std::isalpha(c)) {
+            // consume first char
+            buf.push_back(c);
+            i++;
+            // as long as it keeps reading in a letter or number, keep reading into the buffer
+            while (std::isalnum(str.at(i))) {
+                buf.push_back(str.at(i));
+                i++;
+            }
+            i--;
+
+            // check if buffer is a keyword
+            if (buf == "return") {
+                tokens.push_back({.type = TokenType::_return});
+                buf.clear();
+                continue;
+            } 
+            
+            // if the buffer isn't a keyword, it must be an identifier
+            else {
+                // for now, just return err if not keyword
+                std::cerr << "Expected keyword..." << std::endl;
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        // if the character is a digit, it must be a number (int, float...)
+        // at this point, only ints
+        else if (std::isdigit(c)) {
+            // same logic for reading in keywords / identifiers
+            buf.push_back(c);
+            i++;
+            while (std::isdigit(str.at(i))) {
+                buf.push_back(str.at(i));
+                i++;
+
+            }
+            i--;
+            tokens.push_back({.type = TokenType::int_lit, .value = buf});
+            buf.clear();
+        }
+
+        // basic misc char recognition. pretty self explainatory
+        else if (c == ';') {
+            tokens.push_back({.type = TokenType::semi});
+        }
+
+        // clear doesn't care about whitespace, so it just skips if it encounters any whitespace chars
+        else if (std::isspace(c)) {
+            continue;
+        }
+
+        // char wasn't anything i accounted for, so i guess it's illegal
+        else {
+            std::cerr << "Invalid character" << std::endl;
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    // return the list of all tokens extracted from the source code
+    return tokens;
+}
+
+// temporary function to convert tokens to assambly
+std::string tokens_to_asm(const std::vector<Token>& tokens) {
+    std::stringstream output;
+    output << "global _start\n_start:\n";
+
+    for (int i = 0; i < tokens.size(); i++) {
+        const Token& token = tokens.at(i);
+
+        if (token.type == TokenType::_return) {
+            if (i + 1 < tokens.size() && tokens.at(i+1).type == TokenType::int_lit) {
+                if (i + 2 < tokens.size() && tokens.at(i+2).type == TokenType::semi) {
+                    output << "    mov rax, 60\n";
+                    output << "    mov rdi, " << tokens.at(i+1).value.value() << "\n";
+                    output << "    syscall";
+                }
+            }
+        }
+    }
+
+    return output.str();
+}
+
+int main(int argc, char* argv[]) {
+    // expects an additional command-level argument referencing path to the clear script to run
+    if (argc != 2) {
+        std::cerr << "Incorrect call" << std::endl;
+        std::cerr << "Example: ./clear <../example_script.clr>" << std::endl;
+        return EXIT_FAILURE;
+    }
+    
+    // read entire source file into a stream, then into a string
+    std::string contents;
+    {
+        std::stringstream contents_stream;
+        std::fstream input(argv[1], std::ios::in); 
+        contents_stream << input.rdbuf();
+        contents = contents_stream.str();
+    }
+
+    // convert source string to tokens using the tokenize function
+    std::vector<Token> tokens = tokenize(contents);
+
+    {
+        std::fstream file("out.asm", std::ios::out);
+        file << tokens_to_asm(tokens);
+    }
+
+    system("nasm -felf64 out.asm");
+    system("ld -o out out.o");
+
+    return EXIT_SUCCESS;
+}
