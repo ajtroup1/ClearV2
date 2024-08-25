459e6424739d1d80c555b39a78cbff020b075b31
ajtroup1 <ajtroup1@crimson.ua.edu>
2024-08-24 20:40:51 -0500

    initial arena allocator

--- build/out.asm
+++ build/out.asm
@@ -1,9 +1,9 @@
 global _start
 _start:
-    mov rax, 14
+    mov rax, 27
     push rax
     push QWORD [rsp + 0]
-    push QWORD [rsp + 0]
+    push QWORD [rsp + 8]
     mov rax, 60
     pop rdi
     syscall
